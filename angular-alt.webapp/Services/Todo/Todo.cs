ed8512b440bf6dbe5ccef1eb94b070c03e0ee507
Thomas Brian <tdbrian@gmail.com>
2015-07-18 13:11:34 -0500

    Move service folder and add mongodb dependencies

---
+++ angular-alt.webapp/Services/Todo/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AngularAltTodo.Todo
+{
+	public class Todo
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public bool Completed { get; set; }
+
+		public static TodoDto ToTodoDto(Todo todo)
+		{
+			return new TodoDto()
+			{
+				Id = todo.Id.ToString(),
+				Title = todo.Title,
+				Completed = todo.Completed
+			};
+		}
+	}
+}
---
+++ angular-alt.webapp/Services/Todo/TodoRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace AngularAltTodo.Todo
+{
+	public class TodoStore
+	{
+		private readonly IMongoCollection<Todo> _collection;
+
+		public TodoStore()
+		{
+			var client = new MongoClient("mongodb://localhost:27017");
+			_collection = client.GetDatabase("AngularAltTodo").GetCollection<Todo>("Todo");
+		}
+
+		public async Task<List<Todo>> GetTodos()
+		{
+			return await _collection.Find(_ => true).ToListAsync();
+		}
+
+		public async void AddNewTodo(Todo todo)
+		{
+			await _collection.InsertOneAsync(todo);
+		}
+
+		public async void MarkCompleted(Guid todoId)
+		{
+			await _collection.FindOneAndUpdateAsync<Todo>(
+				(x) => x.Id == todoId,
+				Builders<Todo>.Update.Set(x => x.Completed, true)
+			);
+		}
+	}
+
+}
